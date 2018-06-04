4ec349920ecc5e90cfec47277fae00e153ec4046
Алексей <tralze@mail.ru>
2018-06-04 21:07:10 +0700

    55

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -4,5 +4,33 @@
  * @param {*} value - значение, которе ищем
  * @returns {Object}
  */
-function find (obj, value) {
+function find( obj, searchValue, pKeys ) {
+	let keys = [];
+	
+	for( let key in obj ) {
+		let value = obj[ key ];
+		
+		if( value === searchValue ) {
+			keys.push( pKeys ? `${pKeys}.${key}` : key );
+			continue;
+		}
+
+		if( typeof value !== 'object' || Array.isArray( value ) )
+			continue;
+
+		if( pKeys )
+			key = `${pKeys}.${key}`;
+		
+		let res = find( value, searchValue, key );
+		
+		if( res === null )
+			continue;
+		
+		if( Array.isArray( res ) )
+			keys = keys.concat( res );
+		else
+			keys.push( res );
+	}
+
+	return ( keys.length == 1 ) ? keys[ 0 ] : ( !keys.length ) ? null : keys;
 }
